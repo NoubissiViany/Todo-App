2865a75d4f296be4026c81d339e40664496b44b4
NNVG <g696607013@gmail.com>
2023-06-28 16:16:20 +0100

    full-stack-nextjs-with-typescript-and-firebase-database

--- pages/add-todo.tsx
+++ pages/add-todo.tsx
@@ -1,113 +1,101 @@
-import { doc } from '@firebase/firestore';
-import { setDoc } from 'firebase/firestore';
-import type { NextPage } from 'next'
-import Head from "next/head";
-import { useState } from 'react';
-import { firestore } from '../firebase/clientApp';
-import styles from '../styles/Home.module.css'
+import { doc } from "@firebase/firestore";
+import { setDoc } from "firebase/firestore";
+import type { NextPage } from "next";
+import { useState } from "react";
+import { firestore } from "../firebase/clientApp";
+import styles from "../styles/Home.module.css";
+import Link from "next/link";
 
-const AddTodo:NextPage = () => {
+const AddTodo: NextPage = () => {
+  const [title, setTitle] = useState<string>(""); // title
+  const [description, setDescription] = useState<string>(""); // description
+  const [error, setError] = useState<string>(""); // error
+  const [message, setMessage] = useState<string>(""); // message
 
-    const [title,setTitle] = useState<string>(""); // title
-    const [description,setDescription] = useState<string>("");// description
-    const [error,setError] = useState<string>("");// error
-    const [message,setMessage] = useState<string>("");// message
+  const handleSubmit = (e: { preventDefault: () => void }) => {
+    e.preventDefault(); // avoid default behaviour
 
-    const handleSubmit = (e: { preventDefault: () => void; }) => {
-        e.preventDefault(); // avoid default behaviour
-        
-        if(!title || !description){ // check for any null value
-            return setError("All fields are required");
-        }
-        addTodo();
+    if (!title || !description) {
+      // check for any null value
+      return setError("All fields are required");
     }
+    addTodo();
+  };
 
-    const addTodo = async () => {
-        // get the current timestamp
-        const timestamp:string = Date.now().toString();
-        // create a pointer to our document
-        const _todo = doc(firestore,`todos/${timestamp}`);  
-        // structure the todo data
-        const todoData = {
-            title,
-            description,
-            done:false
-        };
-        
-        try{
-            //add the document
-            await setDoc(_todo,todoData);
-            //show a success message
-            setMessage("Todo added successfully");
-            //reset fields
-            setTitle("");
-            setDescription("");
-        }catch(error){
-            //show an error message
-            setError("An error occurred while adding todo");      
-        }
+  const addTodo = async () => {
+    // get the current timestamp
+    const timestamp: string = Date.now().toString();
+    // create a pointer to our document
+    const _todo = doc(firestore, `todos/${timestamp}`);
+    // structure the todo data
+    const todoData = {
+      id: Date.now(),
+      title,
+      description,
+      done: false,
     };
 
-    return (
-        <div className={styles.container}>
-            <Head>
-                <title>Add todo</title>
-                <meta name="description" content="Next.js firebase todos app" />
-                <link rel="icon" href="/favicon.ico" />
-            </Head>
-            <div className={styles.main}>
+    try {
+      //add the document
+      await setDoc(_todo, todoData);
+      //show a success message
+      setMessage("Todo added successfully");
+      setError("");
+      //reset fields
+      setTitle("");
+      setDescription("");
+    } catch (error) {
+      //show an error message
+      setError("An error occurred while adding todo");
+    }
+  };
 
-                <h1 className={styles.title}>
-                    Add todo
-                </h1>
+  return (
+    <div className={styles.container}>
+      <div className={styles.main}>
+        <h1 className={styles.title}>Add Todo</h1>
 
-                <form onSubmit={handleSubmit} className={styles.form}>
-
-                    {
-                        error ? (
-                            <div className={styles.formGroup}>
-                                <p className={styles.error}>{error}</p>
-                            </div>
-                        ) : null
-                    }
-
-                    {
-                        message ? (
-                            <div className={styles.formGroup}>
-                                <p className={styles.success}>
-                                    {message}. Proceed to <a href="/">Home</a>
-                                </p>
-                            </div>
-                        ) : null
-                    }
-
-                    <div className={styles.formGroup}>
-                        <label>Title</label>
-                        <input type="text" 
-                        placeholder="Todo title" 
-                        onChange={e => setTitle(e.target.value)}
-                        value={title}
-                        />
-                    </div>
-
-                    <div className={styles.formGroup}>
-                        <label>Description</label>
-                        <textarea 
-                        placeholder="Todo description"  
-                        onChange={e => setDescription(e.target.value)}
-                        value={description}
-                        />
-                    </div>
-
-                    <div className={styles.formGroup}>
-                        <button type="submit">Submit</button>
-                    </div>
-
-                </form>
-
+        <form onSubmit={handleSubmit} className={styles.form}>
+          {error ? (
+            <div className={styles.formGroup}>
+              <p className={styles.error}>{error}</p>
             </div>
-        </div>
-    )
-}
+          ) : null}
+
+          {message ? (
+            <div className={styles.formGroup}>
+              <p className={styles.success}>
+                {message}. Proceed to <Link href="/">Home</Link>
+              </p>
+            </div>
+          ) : null}
+
+          <div className={styles.formGroup}>
+            <label>Title</label>
+            <input
+              type="text"
+              placeholder="Todo title"
+              onChange={(e) => setTitle(e.target.value)}
+              value={title}
+            />
+          </div>
+
+          <div className={styles.formGroup}>
+            <label>Description</label>
+            <textarea
+              placeholder="Todo description"
+              onChange={(e) => setDescription(e.target.value)}
+              value={description}
+            />
+          </div>
+
+          <div className={styles.formGroup}>
+            <button type="submit">Submit</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
 
 export default AddTodo;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,121 +1,151 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import { firestore } from '../firebase/clientApp'
-import { collection, deleteDoc, doc, DocumentData, getDocs, limit, query, QueryDocumentSnapshot, updateDoc, where } from "@firebase/firestore";
-import styles from '../styles/Home.module.css'
-import { useEffect, useState } from 'react';
+import type { NextPage } from "next";
+import { MdDelete } from "@react-icons/all-files/md/MdDelete";
+
+import { firestore } from "../firebase/clientApp";
+import {
+  collection,
+  deleteDoc,
+  doc,
+  DocumentData,
+  getDocs,
+  limit,
+  query,
+  QueryDocumentSnapshot,
+  updateDoc,
+  where,
+} from "@firebase/firestore";
+import styles from "../styles/Home.module.css";
+import { useEffect, useState } from "react";
+import Link from "next/link";
 
 const Home: NextPage = () => {
+  const [todos, setTodos] = useState<QueryDocumentSnapshot<DocumentData>[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [check, setCheck] = useState<boolean>(true);
 
-  const [todos,setTodos] = useState<QueryDocumentSnapshot<DocumentData>[]>([]);
-  const [loading,setLoading] = useState<boolean>(true);
-
-  useEffect( () => {
-    getTodos();
-    setTimeout( () => {
-      setLoading(false);
-    },2000)
-    
-  },[]);
-
-
-  const todosCollection = collection(firestore,'todos');
+  const todosCollection = collection(firestore, "todos");
 
   const getTodos = async () => {
-    const todosQuery = query(todosCollection,where('done','==',false),limit(10));
+    const todosQuery = query(todosCollection, limit(10));
     const querySnapshot = await getDocs(todosQuery);
     const result: QueryDocumentSnapshot<DocumentData>[] = [];
     querySnapshot.forEach((snapshot) => {
       result.push(snapshot);
-    })
+    });
     setTodos(result);
   };
 
-  const updateTodo = async (documentId: string) => {
+  const updateTodo1 = async (documentId: string) => {
     // create a pointer to the document id
-    const _todo = doc(firestore,`todos/${documentId}`);
+    const _todo = doc(firestore, `todos/${documentId}`);
 
     // update the doc by setting done to true
-    await updateDoc(_todo,{
-      "done":true
+    await updateDoc(_todo, {
+      done: true,
     });
 
     // retrieve todos
     getTodos();
-  }
+  };
 
-  const deleteTodo = async (documentId:string) => {
-     // create a pointer to the document id
-     const _todo = doc(firestore,`todos/${documentId}`);
+  const updateTodo2 = async (documentId: string) => {
+    // create a pointer to the document id
+    const _todo = doc(firestore, `todos/${documentId}`);
 
-     // delete the doc
-     await deleteDoc(_todo);
- 
-     // retrieve todos
-     getTodos();
-  }
+    // update the doc by setting done to true
+    await updateDoc(_todo, {
+      done: false,
+    });
+
+    // retrieve todos
+    getTodos();
+  };
+
+  const deleteTodo = async (documentId: string) => {
+    // create a pointer to the document id
+    const _todo = doc(firestore, `todos/${documentId}`);
+
+    // delete the doc
+    await deleteDoc(_todo);
+
+    // retrieve todos
+    getTodos();
+  };
+
+  useEffect(() => {
+    getTodos();
+    setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+  });
 
   return (
     <div className={styles.container}>
-      <Head>
-        <title>Todos app</title>
-        <meta name="description" content="Next.js firebase todos app" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
       <main className={styles.main}>
-
-        <h1 className={styles.title}>
-          Todos app
-        </h1>
+        <h1 className={styles.title}>Todo App</h1>
 
         <div className={styles.grid}>
-          {
-            loading ? (
-              <div className={styles.card}>
-                <h2>Loading</h2>
-              </div>
-            ): 
-            todos.length === 0 ? (
-              <div className={styles.card}>
-                <h2>No undone todos</h2>
-                <p>Consider adding a todo from <a href="/add-todo">here</a></p>
-              </div>
-            ) : (
-              todos.map((todo,index) => {
+          {loading ? (
+            <div className={styles.card}>
+              <h2>Loading...</h2>
+            </div>
+          ) : todos.length === 0 ? (
+            <div className={styles.card}>
+              <h2>No todos</h2>
+            </div>
+          ) : (
+            <div className={styles.card}>
+              {todos.map((todo) => {
                 return (
-                  <div className={styles.card} key={index}> 
-
-                    <h2>{todo.data().title}</h2>
-                    <p>{todo.data().description}</p>
-
-                    <div className={styles.cardActions}>
-
-                    <button type="button" onClick={() => updateTodo(todo.id)}>Mark as done</button>
-
-                    <button type="button" onClick={() => deleteTodo(todo.id)}>Delete</button>
-
+                  <div className={styles.cardContainer} key={todo.id}>
+                    {todo.data().done == false ? (
+                      <input
+                        type="radio"
+                        onClick={() => updateTodo1(todo.id)}
+                      />
+                    ) : (
+                      <input
+                        type="radio"
+                        checked
+                        onClick={() => updateTodo2(todo.id)}
+                      />
+                    )}
+                    <div
+                      className={
+                        todo.data().done == true
+                          ? styles.strickHeading
+                          : styles.cardHeading
+                      }
+                    >
+                      <h2>{todo.data().title}: </h2>
+                      <p>{todo.data().description}</p>
                     </div>
-                    
+                    <MdDelete
+                      className={styles.cardIcon}
+                      onClick={() => deleteTodo(todo.id)}
+                    />
                   </div>
-                )
-              })
-            )
-          }
+
+                  /* <div className={styles.cardActions}>
+                        <button type="button" onClick={() => updateTodo(todo.id)}>
+                          Mark as done
+                        </button>
+    
+                        <button type="button" onClick={() => deleteTodo(todo.id)}>
+                          Delete
+                        </button>
+                      </div> */
+                );
+              })}
+            </div>
+          )}
         </div>
-
+        <Link href="/add-todo" className={styles.taskBtn}>
+          + New task
+        </Link>
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="#"
-          rel="noopener noreferrer"
-        >
-          Todos app
-        </a>
-      </footer>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
