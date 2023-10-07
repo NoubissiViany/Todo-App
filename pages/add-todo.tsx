f6fa0e46fd4eb95faeeba9350d4e108d12a342de
NNVG <g696607013@gmail.com>
2023-10-07 15:33:02 +0100

    Solve Merging Conflict

--- pages/add-todo.tsx
+++ pages/add-todo.tsx
@@ -19,7 +19,7 @@ const AddTodo: NextPage = () => {
       // check for any null value
       return setError("All fields are required");
     } else {
-      setError("");
+      setError("a");
     }
     addTodo();
   };
